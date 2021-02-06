77c7b8076ffdb5764eef1b2a05b5bffbf05da5f8
heenasangtani <42833819+heenasangtani@users.noreply.github.com>
2021-02-06 17:39:07 +0530

    navbar design update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div style={{backgroundColor:'#333',height:'92vh'}}>
-         
+
+            <div style={{backgroundColor:'#fff',height:'46vh',width:'20vh'}}>
+
+            </div>
+               <div style={{backgroundColor:'#ddd5',height:'46vh'}}>
+                   </div>        
         </div>
     );
 }
--- src/components/Resheader.js
+++ src/components/Resheader.js
@@ -3,25 +3,38 @@ import './style.css';
 import Logo from '../images/reactlog.png'
 
 const Resheader = () => {
+
+    const navSlide=()=>{
+        const burger = document.querySelector('.burger')
+        const nav= document.querySelector('.nav-link')
+        burger.addEventListener('click',()=>{
+            nav.classList.toggle('nav-active')
+        })
+    }
+
+    const navHandler=()=>{
+        navSlide()
+    }
+
     return (
         <div>
             <nav>
             <img src={Logo} style={{height:30, width:30}}/>
             <div className='logo'>
-    <h4>The Nav</h4>
+    <h4>React Nav</h4>
             </div>
             <ul className='nav-link'>
                 <li>Home</li>
                 <li>Contact</li>
                 <li>About</li>
             </ul>
-            <div class = 'burger'>
+            <div class = 'burger' onClick = {navHandler}>
              <div class = 'line1'></div>
              <div class = 'line2'></div>
              <div class = 'line3'></div>
             </div>
             </nav>
- 
+               
         </div>
     );
 }
